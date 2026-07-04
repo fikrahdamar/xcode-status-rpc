0ed14fd8be0e6a1475f2ee67501f09f7e5fcdb7e
fikrahdamars <fikrahdamar7@gmail.com>
2026-07-05 01:22:01 +0700

    initial setup

---
+++ xcode-status-rpc/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  xcode-status-rpc
+//
+//  Created by Fikrah Damar Huda on 05/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ xcode-status-rpc/xcode_status_rpcApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_status_rpcApp.swift
+//  xcode-status-rpc
+//
+//  Created by Fikrah Damar Huda on 05/07/26.
+//
+
+import SwiftUI
+
+@main
+struct xcode_status_rpcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
